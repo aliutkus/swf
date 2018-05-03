726af1960ed7010a00a1701c1bfd98cc1bfe4074
Antoine Liutkus <antoine.liutkus@inria.fr>
2018-05-03 21:37:47 +0200

    current version starts working on real datasets

--- code/demo.sh
+++ code/demo.sh
@@ -5,4 +5,4 @@
  python qsketch/sketch.py ./toydata.npy -n 50 -q 100 -o toysketch -p RandomLocalizedProjectors
 
 # IDT
- python sketchIDT.py toysketch.npy  -d 2 -n 10000 --plot_target toydata.npy --plot -s 1 -r 0.001 -e 10000 -b 10
+ python sketchIDT.py toysketch.npy  -d 2 -n 3000 --plot_target toydata.npy --plot -s 1 -r 0.001 -e 10000 -b 10
--- code/demo_cifar.sh
+++ code/demo_cifar.sh
@@ -2,5 +2,5 @@
 #
 #python sketchIDT.py ~/sketchCIFAR10.npy  -d 300 -n 3000 --plot -s 100 -r 0.01 -e 10000 -b 5
 
-python qsketch/sketch.py CIFAR10  -n 400 -q 100 -o ~/tmp/localizedCIFAR10 -s 32 -p RandomLocalizedProjectors -m 1 -r qsketch/data/CIFAR10
+python qsketch/sketch.py CIFAR10  -n 800 -q 100 -o ~/tmp/localizedCIFAR10 -s 32 -p RandomLocalizedProjectors -m 1 -r qsketch/data/CIFAR10
 python sketchIDT.py ~/tmp/localizedCIFAR10.npy  -d 300 -n 3000 --plot -s 1000 -r 0.001 -e 10000 -b 5
