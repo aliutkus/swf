39429f57a959c4b2639bf544a4759c8c43b17f68
Antoine Liutkus <antoine.liutkus@inria.fr>
2018-05-05 08:38:00 +0200

    current version

--- code/demo_cifar.sh
+++ code/demo_cifar.sh
@@ -2,5 +2,5 @@
 #
 #python sketchIDT.py ~/sketchCIFAR10.npy  -d 300 -n 3000 --plot -s 100 -r 0.01 -e 10000 -b 5
 
-#python qsketch/sketch.py CIFAR10  -n 1000 -q 100 -o ~/tmp/CIFAR10 -s 32 -p RandomProjectors -m 1 -r qsketch/data/CIFAR10
-python sketchIDT.py ~/tmp/CIFAR10.npy  -d 300 -n 3000 --plot -s 1000 -r 0.001 -e 10000 -b 5
+python qsketch/sketch.py CIFAR10  -n 1000 -q 100 -o ~/tmp/localizedCIFAR10 -s 32 -p RandomLocalizedProjectors -m 1 -r qsketch/data/CIFAR10
+python sketchIDT.py ~/tmp/localizedCIFAR10.npy  -d 300 -n 3000 --plot -s 1000 -r 0.001 -e 10000 -b 10 -p samples_localized
