d23c7a7784eb1a509a6e4f41edec538739555af9
Antoine Liutkus <antoine.liutkus@inria.fr>
2018-05-09 08:27:39 +0200

    current version

--- code/demo_fmnist.py
+++ code/demo_fmnist.py
@@ -4,4 +4,4 @@
 
 
 # streaming version
-python streamIDT.py FashionMNIST  --memory_usage 1 --root_data_dir qsketch/data/FashionMNIST --num_quantiles 200  --projectors RandomLocalizedProjectors  --input_dim 500 --num_samples 3000  --plot --stepsize 100 --reg 0.0001 --batchsize 5 --plot_dir samples_fmnist_localized
+python streamIDT.py FashionMNIST  --memory_usage 1 --root_data_dir qsketch/data/FashionMNIST --num_quantiles 200  --projectors RandomProjectors  --input_dim 500 --num_samples 3000  --plot --stepsize 100 --reg 0.0001 --batchsize 5 --plot_dir samples_fmnist_stream
