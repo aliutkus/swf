a40fd23f9d83f66d211306d67019ec2037ed1539
Antoine Liutkus <antoine.liutkus@inria.fr>
2019-01-23 00:19:37 +0100

    modified demo.sh

--- code/demo.sh
+++ code/demo.sh
@@ -3,7 +3,7 @@ NUM_SKETCHES=1
 NUM_QUANTILES=100
 # pick something related to bottleneck size for num_thetas
 NUM_THETAS=16000
-CLIPTO=3000
+CLIPTO=30000
 
 # parameters for auto encoder
 BOTTLENECK_SIZE=80
@@ -14,6 +14,7 @@ CONV_AE_STRING="--conv_ae"
 # pick something like num_thetas/4
 STEPSIZE=1000
 REG=0
+NUM_ITER=10000
 
 # parameters for particles
 NUM_SAMPLES=3000
@@ -42,4 +43,4 @@ if [ $1 = "toy.npy" ]; then
 fi
 
 # now launch the sliced Wasserstein flow
-python swf.py $1 --root_data_dir ~/data/$1 --img_size 32 --num_sketches $NUM_SKETCHES --clip $CLIPTO --num_quantiles $NUM_QUANTILES --input_dim $INPUT_DIM  --num_samples $NUM_SAMPLES --particles_type=$PARTICLES_TYPE --stepsize $STEPSIZE --num_thetas $NUM_THETAS --plot_dir ~/swf_samples_$1 --logdir logs/$1 --plot_every $PLOT_EVERY --match_every $MATCH_EVERY $AE_STRING $CONV_AE_STRING --bottleneck_size $BOTTLENECK_SIZE --ae_model ae --num_test $NUM_TEST --test_type $TEST_TYPE
+python swf.py $1 --root_data_dir ~/data/$1 --img_size 32 --num_sketches $NUM_SKETCHES --clip $CLIPTO --num_quantiles $NUM_QUANTILES --input_dim $INPUT_DIM  --num_samples $NUM_SAMPLES --particles_type=$PARTICLES_TYPE --stepsize $STEPSIZE --num_thetas $NUM_THETAS --plot_dir ~/swf_samples_$1 --logdir logs/$1 --plot_every $PLOT_EVERY --num_iter $NUM_ITER --match_every $MATCH_EVERY $AE_STRING $CONV_AE_STRING --bottleneck_size $BOTTLENECK_SIZE --ae_model ae --num_test $NUM_TEST --test_type $TEST_TYPE
