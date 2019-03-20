58f97c20ef3b4b4fc76784896742b271161d1349
Antoine Liutkus <antoine.liutkus@inria.fr>
2019-03-20 15:04:19 +0100

    demo script

--- code/demo.sh
+++ code/demo.sh
@@ -1,19 +1,19 @@
 # parameters for sketching
-NUM_SKETCHES=10
+NUM_SKETCHES=100
 NUM_QUANTILES=200
 # pick something related to bottleneck size for num_thetas
-NUM_THETAS=5000
+NUM_THETAS=200
 CLIPTO=3000
 
-IMG_SIZE=64
+IMG_SIZE=32
 # parameters for auto encoder
 BOTTLENECK_SIZE=128
-AE_STRING="--ae"
+AE_STRING=""
 CONV_AE_STRING="--conv_ae"
 
 # parameters for SWF
 # pick something like num_thetas/4
-STEPSIZE=100
+STEPSIZE=10
 REG=0
 NUM_EPOCHS=10001
 
@@ -44,4 +44,4 @@ if [ $1 = "toy.npy" ]; then
 fi
 
 # now launch the sliced Wasserstein flow
-python -m cProfile -o 100_percent_gpu_utilization.prof swf.py $1 --root_data_dir ~/data --img_size $IMG_SIZE --num_sketches $NUM_SKETCHES --clip $CLIPTO --num_quantiles $NUM_QUANTILES --input_dim $INPUT_DIM  --num_samples $NUM_SAMPLES --particles_type=$PARTICLES_TYPE --stepsize $STEPSIZE --num_thetas $NUM_THETAS --plot_dir ~/swf_samples_$1 --logdir logs/$1 --plot_every $PLOT_EVERY --num_epochs $NUM_EPOCHS --match_every $MATCH_EVERY $AE_STRING $CONV_AE_STRING --bottleneck_size $BOTTLENECK_SIZE --ae_model ae --num_test $NUM_TEST --test_type $TEST_TYPE
+python -m cProfile -o 100_percent_gpu_utilization.prof swf.py $1 --num_workers 15 --root_data_dir ~/data/$1 --img_size $IMG_SIZE --num_sketches $NUM_SKETCHES --clip $CLIPTO --num_quantiles $NUM_QUANTILES --input_dim $INPUT_DIM  --num_samples $NUM_SAMPLES --particles_type=$PARTICLES_TYPE --stepsize $STEPSIZE --num_thetas $NUM_THETAS --plot_dir ~/swf_samples_$1 --plot_every $PLOT_EVERY --num_epochs $NUM_EPOCHS --match_every $MATCH_EVERY $AE_STRING $CONV_AE_STRING --bottleneck_size $BOTTLENECK_SIZE --ae_model ae --num_test $NUM_TEST --test_type $TEST_TYPE
