fe33c86967388e590cb9e5fd67e82aced6788881
Antoine Liutkus <antoine.liutkus@inria.fr>
2019-03-20 14:32:50 +0100

    Revert "merging"
    This reverts commit 62661f6ddae0180724e2298f44fb5a5b3ae8f964, reversing
    changes made to eab0a04758e45ee2ff9f46e0690cc5fc4872434c.

--- code/sketch.py
+++ code/sketch.py
@@ -12,7 +12,6 @@ import time
 
 
 class Projectors:
-<<<<<<< HEAD
     """Dataset class for projectors"""
 
     def __init__(self, num_thetas, data_shape, projector_class):
@@ -27,16 +26,10 @@ class Projectors:
             is a shape tensor, and `out_features`, which is integer.
 
         """
-=======
-    """Each projector is a set of unit-length random vector"""
-
-    def __init__(self, num_thetas, data_shape, projector_class, device='cpu'):
->>>>>>> 4289ffaa6b21858b75e5289c425b4e5afb891ccf
         self.num_thetas = num_thetas
         self.data_shape = data_shape
         self.data_dim = np.prod(np.array(data_shape))
         self.projector_class = projector_class
-<<<<<<< HEAD
 
         # for now, always use the CPU for generating projectors
         self.device = "cpu"
@@ -54,63 +47,7 @@ class Projectors:
                             in_features=self.data_shape,
                             out_features=self.num_thetas).to(self.device)]
         return result[0] if isinstance(indexes, int) else result
-=======
-        self.device = device
 
-    def __getitem__(self, indexes):
-        device = torch.device(self.device)
-
-        if isinstance(indexes, int):
-            idx = [indexes]
-        else:
-            idx = indexes
-
-        result = []
-        for pos, id in enumerate(idx):
-            torch.manual_seed(id)
-            result += [self.projector_class(
-                                      in_features=self.data_dim,
-                                      out_features=self.num_thetas).to(device)]
-        return result[0] if isinstance(indexes, int) else result
-
-def RandomIterator():
-    def __init__(self, nitems=None, seed=0):
-        # the random sequence should be reproductible without interfering
-        # with other np random seed stuff. To achieve this, we
-        # backup current numpy random state, and create a new one
-        # based on the provided seed. Then, we revert to the random state that
-        # was there.
-        random_state = np.random.get_state()
-        np.random.seed(seed)
-        self.random_state = np.random.get_state()
-        np.random.set_state(random_state)
-
-        self.nitems = nitems
-        self.pos = 0
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        self.pos += 1
-        if self.nitems is not None and self.pos >= self.nitems:
-            # there is a fixed number of items, send the None sentinel
-            return None
-
-        # saves the current numpy random state, before setting it to the last
-        # one stored
-        random_state = np.random.get_state()
-        np.random.set_state(self.random_state)
-
-        # generate the next id
-        next_id = np.random.randint(np.iinfo(np.int32).max)
->>>>>>> 4289ffaa6b21858b75e5289c425b4e5afb891ccf
-
-        # stores current numpy random state and restores the one we had before
-        self.random_state = np.random.get_state()
-        np.random.set_state(random_state)
-
-        return next_id
 
 class Sketcher(Dataset):
     """Sketcher class: takes a source of data, a dataset of projectors, and
@@ -142,18 +79,34 @@ class Sketcher(Dataset):
         self.num_quantiles = num_quantiles
         self.clip_to = len(data_stream.dataset) if clip_to < 0 else clip_to
 
+        # the random sequence should be reproductible without interfering
+        # with other np random seed stuff. To achieve this, we
+        # backup current numpy random state, and create a new one
+        # based on the provided seed. Then, we revert to the random state that
+        # was there.
+        random_state = np.random.get_state()
+        np.random.seed(seed)
+        self.random_state = np.random.get_state()
+        np.random.set_state(random_state)
+
         # only use the cpu for now for the sketcher
         self.device = "cpu"
-    
-        # create a random sequence with provided seed
-        self.counter = RandomIterator(seed) 
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        # get the next id
-        next_id = next(self.counter)
+        # saves the current numpy random state, before setting it to the last
+        # one stored
+        random_state = np.random.get_state()
+        np.random.set_state(self.random_state)
+
+        # generate the next id
+        next_id = np.random.randint(np.iinfo(np.int32).max)
+
+        # stores current numpy random state and restores the one we had before
+        self.random_state = np.random.get_state()
+        np.random.set_state(random_state)
 
         # finally get the sketch
         return self.__getitem__(next_id)
@@ -193,7 +146,7 @@ class Sketcher(Dataset):
                 (Percentile(
                     self.num_quantiles, device)(projections).float(),
                  projector,
-                 id)]
+                 index)]
         return sketches[0] if isinstance(indexes, int) else sketches
 
 
@@ -243,26 +196,22 @@ class SketchStream:
             num_workers = max(1, min(num_workers,
                               int((mp.cpu_count()-1)/2)))
 
-        print('SketchStream using ', num_workers, 'workers')
+        print('using ', num_workers, 'workers')
         # now create a queue with a maxsize corresponding to a few times
         # the number of workers
         self.queue = self.ctx.Queue(maxsize=2*num_workers)
         self.manager = self.ctx.Manager()
         self.num_epochs = num_epochs
-        self.num_sketches = (num_sketches if num_sketches > 0
-                             else np.inf)
         self.projectors = projectors
-
-        # prepare some data for the synchronization of the workers
         self.data = self.manager.dict()
         self.data['pause'] = False
         self.data['current_pick_epoch'] = 0
         self.data['current_put_epoch'] = 0
         self.data['current_sketch'] = 0
         self.data['done_in_current_epoch'] = 0
+        self.data['num_sketches'] = (num_sketches if num_sketches > 0
+                                     else np.inf)
         self.lock = self.ctx.Lock()
-
-        # prepare the workers
         self.processes = [self.ctx.Process(target=sketch_worker,
                                            kwargs={'sketcher':
                                                    Sketcher(data_stream.queue,
@@ -274,7 +223,6 @@ class SketchStream:
 
         atexit.register(partial(exit_handler, stream=self))
 
-        # go
         for p in self.processes:
             p.start()
 
@@ -306,13 +254,8 @@ def exit_handler(stream):
 
 
 def sketch_worker(sketcher, stream):
-    """ Actual worker for the sketch stream.
-    Will get sketch ids, get data from the data queue and put sketches in the
-    stream queue"""
-
     @contextmanager
     def getlock():
-        # get the lock of the stream to manipulate the stream.data
         result = stream.lock.acquire(block=True)
         yield result
         if result:
@@ -320,84 +263,55 @@ def sketch_worker(sketcher, stream):
 
     pause_displayed = False
     while True:
-        # not dying, unless we see that later
         worker_dying = False
-
         if not stream.data['pause']:
-            # not in pause
-
             if pause_displayed:
-                # we were in pause previously. Output that we're no more
                 print('Sketch worker back from sleep')
                 pause_displayed = False
 
-            # print('sketch: trying to get lock')
-            # With the lock, so that only one worker can manipulate the
-            # counting in the stream data at the same time.
+            #print('sketch: trying to get lock')
             with getlock():
-                # get both the id to compute, and the number of the pick_epoch
-                # (the epoch we are currently asked to compute, as opposed
-                # to the put epoch, which is the epoch whose sketches we are
-                # currently putting in the queue.)
                 id = stream.data['current_sketch']
                 epoch = stream.data['current_pick_epoch']
-                # print('sketch: got lock, epoch %d and id %d' % (epoch, id))
+                #print('sketch: got lock, epoch %d and id %d' % (epoch, id))
                 if epoch >= stream.num_epochs:
-                    # the picked epoch is larger than the number of epochs.
-                    # sketching is finished.
-                    print('epoch', epoch, 'greater than the number of epochs:',
-                          stream.num_epochs, 'dying now.')
+                    print('epoch',epoch,'greater than the number of epochs:',stream.num_epochs,'dying now')
                     worker_dying = True
                 else:
-                    if id == stream.num_sketches - 1:
-                        # we reached the number of sketches per epoch.
-                        # we let the other workers know and increment the
-                        # pick epoch.
+                    if id == stream.data['num_sketches'] - 1:
+                        # we reached the limit, we let the other workers know
                         print("Obtained id %d is last for this epoch. "
                               "Reseting the counter and incrementing current "
                               "epoch " % id)
                         stream.data['current_sketch'] = 0
                         stream.data['current_pick_epoch'] += 1
                     else:
-                        # we just increment the current sketch to pick for
-                        # the next worker.
                         stream.data['current_sketch'] += 1
             if worker_dying:
-                # dying has been asked for. we'll just loop infinitely.
-                # this is because there is apparently some issues raised when
-                # we just kill the worker, in case some data in the queue
-                # originated from him has not been taken out ?
-                print(
-                    id, epoch, 'Reached the desired amount of epochs. Dying.')
+                print(id, epoch, 'Reached the desired amount of epochs. Dying.')
                 while True:
                     time.sleep(10)
                 return
 
-            # now to the thing. We compute the sketch that has been asked for.
             #print('sketch: now trying to compute id', id)
             (target_qf, projector, id) = sketcher[id]
 
-            # print('sketch: we computed the sketch with id', id)
-            # we need to wait until the current put epoch is the epoch we
-            # picked. It may indeed happen that we are several epochs ahead.
+            #print('sketch: we computed the sketch with id', id)
             while (stream.data['current_put_epoch'] != epoch):
                 #print("waiting: current put epoch",stream.data['current_put_epoch'])
-                time.sleep(1)
                 pass
 
             #print('sketch: trying to put id',id,'epoch',epoch)
-            # now we actually put the sketch in the queue.
-            stream.queue.put((target_qf.detach(), id))
+            stream.queue.put(((target_qf.detach(), projector, id)))
             #print('sketch: we put id', id, 'epoch', epoch)
 
-            with getlock():
-                # we put the data, now update the counting
+            with stream.lock:
                 stream.data['done_in_current_epoch'] += 1
                 #print('sketch: after put, got lock. id', id, 'epoch', epoch, 'done in current epoch',stream.data['done_in_current_epoch'])
                 if (
                   stream.data['done_in_current_epoch']
-                  == stream.num_sketches):
-                    # This item was the last of its epoch, we put the sentinel
+                  == stream.data['num_sketches']):
+                    # we need to send sentinel
                     print('Sketch: sending the sentinel')
                     stream.queue.put(None)
                     stream.data['done_in_current_epoch'] = 0
